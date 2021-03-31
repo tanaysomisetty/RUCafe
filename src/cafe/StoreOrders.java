3dfc33b8e872f310e109bcef63dbc9ad8769c9bb
sailokesh845 <70788658+sailokesh845@users.noreply.github.com>
2021-03-31 14:12:54 -0400

    Completed add and remove for StoreOrders.java

--- src/cafe/StoreOrders.java
+++ src/cafe/StoreOrders.java
@@ -1,11 +1,29 @@
 package cafe;
 
+import java.util.ArrayList;
+
 public class StoreOrders implements Customizable {
+    private ArrayList orderList = new ArrayList();
 
     public boolean add(Object obj) {
+        if (obj instanceof StoreOrders) {
+            orderList.add(obj);
+            return true;
+        }
         return false;
     }
+
+
     public boolean remove(Object obj) {
+        if (obj instanceof StoreOrders) {
+            StoreOrders orders = (StoreOrders) obj;
+            int storeOrderIndex =orderList.indexOf(orders);
+            if (storeOrderIndex < 0) {
+                return false;
+            }
+            orderList.remove(storeOrderIndex);
+            return true;
+        }
         return false;
     }
 }
